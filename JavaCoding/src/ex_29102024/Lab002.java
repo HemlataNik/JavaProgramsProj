dba8b4a96a9be7d9288a8dd01af4afd16f853706
HemlataNik <morehemlata36@gmail.com>
2024-10-29 13:05:32 +0530

    Adding Star pattern code

---
+++ JavaCoding/src/ex_29102024/Lab002.java
@@ -0,0 +1,28 @@
+package ex_29102024;
+
+public class Lab002 {
+	public static void main(String[] args) {
+		// Write code to print right triangle star pattern
+		/*
+ * 
+ * * 
+ * * * 
+ * * * * 
+ * * * * * 
+ * * * * * * 
+ * * * * * * *
+ */
+		
+		for(int i = 1; i <= 7; i++)
+		{
+			
+			for (int j = 1; j<=i; j++)
+			{
+				System.out.print("*");
+					
+			}
+			System.out.println();
+		}
+		}
+}
+	
