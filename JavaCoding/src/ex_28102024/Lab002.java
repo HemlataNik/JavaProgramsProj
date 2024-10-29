3ce1122636f25035f89d90c3b45d8359e6b61947
HemlataNik <morehemlata36@gmail.com>
2024-10-29 09:16:34 +0530

    Adding new task files

---
+++ JavaCoding/src/ex_28102024/Lab002.java
@@ -0,0 +1,20 @@
+package ex_28102024;
+
+public class Lab002 {
+	public static void main(String[] args) {
+		//write Java program to find maximum of three numbers
+		int a = 100, b = 300, c = 500;
+		
+		 if (a >= b && a >= c)
+			 
+	         System.out.println( a + " is the maximum number.");
+		 
+	      else if (b >= a && b >= c)
+	    	  
+	         System.out.println( b + " is the maximum number.");
+		 
+	      else
+	    	  
+	         System.out.println( c + " is the maximum number.");
+	   }
+}
